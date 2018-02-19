6032b6f642911fc18f3b4a8e5c42580926c2dfb6
Jeremy <amuesing1@gmail.com>
2018-02-19 13:32:44 -0700

    finished assignment

--- CSCI5822/assignment3/assignment3.py
+++ CSCI5822/assignment3/assignment3.py
@@ -1,3 +1,4 @@
+from __future__ import division
 import math
 import numpy as np
 from scipy.stats import beta
@@ -7,14 +8,29 @@ from matplotlib import pyplot as plt
 x=np.linspace(0,1,101)
 outcome=[1,0,0,1,0,0,0,1]
 alpha=beta_value=1
+i=1
+dist=beta(1,1)
+plt.plot(x,dist.pdf(x))
+plt.xlabel('Model')
+plt.ylim(0,3)
+plt.xlim(0,1)
+plt.title('Priors')
+plt.show()
 for flip in outcome:
     if flip==1:
+        letter='H'
         alpha=alpha+1
     elif flip==0:
+        letter='T'
         beta_value=beta_value+1
     dist=beta(alpha,beta_value)
     plt.plot(x,dist.pdf(x))
+    plt.ylim(0,3)
+    plt.xlim(0,1)
+    plt.xlabel('Model')
+    plt.title('Trial '+str(i)+': '+letter)
     plt.show()
+    i=i+1
 
 # Part II
 exampleA1 = [
@@ -110,7 +126,7 @@ for set_of_examples in list_o_matricies:
     new_like=likelihoods[i]
     new_prior=priors[i]
     for vx in velocities:
-        for vy in velocities:
+        for vy in velocities[::-1]:
             new_prior[velocities.index(vy),velocities.index(vx)]=np.exp(-(pow(vx,2)+pow(vy,2))/(2*sigma))
             for x in range(set_of_examples[0].shape[0]):
                 for y in range(set_of_examples[0].shape[1]):
@@ -130,13 +146,31 @@ x=range(-2,3)
 for j in range(3):
     plt.clf()
     cs=plt.contourf(x,x,np.log(likelihoods[j]),corner_mask=False)
+    if j==0:
+        letter='A'
+    elif j==1:
+        letter='B'
+    elif j==2:
+        letter='C'
     plt.colorbar(cs,ticks=cs.levels)
+    plt.xlabel('X velocity')
+    plt.ylabel('Y velocity')
+    plt.title('Log Likelihood of Motion (unscaled) | Example '+letter)
     plt.show()
 # task 2
 for j in range(3):
     plt.clf()
     cs=plt.contourf(x,x,posteriors[j],corner_mask=False)
+    if j==0:
+        letter='A'
+    elif j==1:
+        letter='B'
+    elif j==2:
+        letter='C'
     plt.colorbar(cs,ticks=cs.levels)
+    plt.xlabel('X velocity')
+    plt.ylabel('Y velocity')
+    plt.title('Log Postieror of Motion (unscaled) | Example '+letter)
     plt.show()
 # task 3
 for j in range(3):
@@ -144,5 +178,46 @@ for j in range(3):
     posteriors[j]=np.divide(posteriors[j],np.sum(posteriors[j]))
     plt.clf()
     cs=plt.contourf(x,x,posteriors[j],corner_mask=False)
+    if j==0:
+        letter='A'
+    elif j==1:
+        letter='B'
+    elif j==2:
+        letter='C'
     plt.colorbar(cs,ticks=cs.levels)
+    plt.xlabel('X velocity')
+    plt.ylabel('Y velocity')
+    plt.title('Postieror of Motion (scaled) | Example '+letter)
     plt.show()
+
+# Part III
+
+def partIII(priors,obs,sigma,prior_title):
+    post=[0.0,0.0,0.0,0.0]
+    directions=[[-1,0],[0,1],[1,0],[0,-1]]
+    for direction in directions: # left[dx,dy], up, right, down
+        pRx=1/(math.sqrt(2*math.pi*sigma**2))*np.exp(-((obs[0]-direction[0])**2)/(2*sigma**2))
+        pRy=1/(math.sqrt(2*math.pi*sigma**2))*np.exp(-((obs[1]-direction[1])**2)/(2*sigma**2))
+        pBx=1/(math.sqrt(2*math.pi*sigma**2))*np.exp(-((obs[2]-direction[0])**2)/(2*sigma**2))
+        pBy=1/(math.sqrt(2*math.pi*sigma**2))*np.exp(-((obs[3]-direction[1])**2)/(2*sigma**2))
+        post[directions.index(direction)]=priors[directions.index(direction)]*pRx*pRy*pBx*pBy
+    post=post/sum(post)
+    plt.clf()
+    plt.bar(['left','up','right','down'],post)
+    plt.xlabel('Direcition of Motion')
+    plt.ylabel('Normalized Probability')
+    plt.title('P(Direction|Rx,Ry,Bx,By) | $\sigma$='+str(sigma)+' | '+prior_title)
+    plt.show()
+
+# task 1
+priors=[1/4,1/4,1/4,1/4]
+obs=[0.75,-0.6,1.4,-0.2]
+partIII(priors,obs,1,'Uniform Priors')
+# task 2
+partIII(priors,obs,5,'Uniform Priors')
+priors=[1/8,1/8,1/8,5/8]
+# task 3
+partIII(priors,obs,1,'Large Down Prior')
+# task 4
+partIII(priors,obs,5,'Large Down Prior')
+
