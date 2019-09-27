bdef1bcaee77b5a71fa2a118a92ad80dc9d08a41
Jeremy <amuesing1@gmail.com>
2019-09-27 17:50:11 -0600

    finished kinematics

--- ASEN5519/hw1/hw1.py
+++ ASEN5519/hw1/hw1.py
@@ -176,16 +176,12 @@ class Bug():
                 self.bug_path[self.loc[0],self.loc[1]]+=1
                 self.bug_path_graph.append(copy.copy(self.loc))
                 still_there=self.check_sides()
-                #  print(self.regular_path[self.loc[0],self.loc[1]],current_d,closest_d,stop)
-                #  print(current_d<closest_d)
                 if (self.regular_path[self.loc[0],self.loc[1]]==1) and (current_d<closest_d):
                     stop=True
 
 
             if not stop:
                 new_move=self.check_corners()
-                #  print(new_move,self.loc)
-                #  print(self.loc)
                 self.loc+=new_move
                 current_d,xd,yd=self.get_distance()
 
@@ -265,9 +261,6 @@ class Bug():
                         self.bug_path_graph.append(copy.copy(self.loc))
                     if self.grid[self.loc[0],self.loc[1]]==10:
                         break
-                    #  self.bug_path[self.loc[0],self.loc[1]]+=1
-        #implement bug 1 algorithm
-        #return the path of the bug through the array
 
     def bug2(self):
         self.loc=np.where(self.grid==5)
@@ -343,20 +336,13 @@ class Bug():
                     if self.grid[self.loc[0],self.loc[1]]==10:
                         stop=True
                         break
-        #implement bug1 algorithm
-        #return the path of the bug through the grid
 
     def graph_path(self,sizing,problem_num):
         fig,ax=plt.subplots()
         for x in range(self.grid.shape[0]):
             for y in range(self.grid.shape[1]):
-                #  if self.grid[x,y]==1:
-                #      ax.scatter(x,y,marker='s',c='black')
                 if self.grid[x,y]==10:
                     ax.scatter(x,y,marker='*')
-                #  if self.bug_path[x,y]>0:
-                #      plt.scatter(x,y,c='C0')
-        #  print(self.bug_path_graph)
         if problem_num==1:
             ob1=Rectangle((1*sizing,1*sizing),1*sizing,4*sizing)
             ob2=Rectangle((3*sizing,4*sizing),1*sizing,8*sizing)
@@ -390,13 +376,178 @@ class Bug():
             ax.add_patch(ob9)
         path=np.array(self.bug_path_graph)
         ax.plot(path[:,0],path[:,1],color='black')
+        ax.axis('equal')
+        ax.axis('off')
+        plt.show()
+
+class Arm():
+    def kinematics(self,len1,len2,len3,ang1,ang2,ang3):
+        ang1=np.deg2rad(ang1)
+        ang2=np.deg2rad(ang2)
+        ang3=np.deg2rad(ang3)
+        #  ang4=np.deg2rad(90)
+        T1=np.array([[np.cos(ang1),-np.sin(ang1),0],
+            [np.sin(ang1),np.cos(ang1),0],
+            [0,0,1]])
+        T2=np.array([[np.cos(ang2),-np.sin(ang2),len1],
+            [np.sin(ang2),np.cos(ang2),0],
+            [0,0,1]])
+        T3=np.array([[np.cos(ang3),-np.sin(ang3),len2],
+            [np.sin(ang3),np.cos(ang3),0],
+            [0,0,1]])
+        T4=np.array([[1,0,len3],
+            [0,1,0],
+            [0,0,1]])
+        #  T4=np.array([[np.cos(ang4),-np.sin(ang4),len3],
+        #      [np.sin(ang4),np.cos(ang4),0],
+        #      [0,0,1]])
+        #  T5=np.array([[1,0,1],
+        #      [0,1,0],
+        #      [0,0,1]])
+        link1_base=np.array([[0],[0],[1]])
+        link1_top=np.matmul(T1,np.matmul(T2,link1_base))
+        link2_base=copy.copy(link1_top)
+        link2_top=np.matmul(T1,np.matmul(T2,np.matmul(T3,link1_base)))
+        link3_base=copy.copy(link2_top)
+        link3_top=np.matmul(T1,np.matmul(T2,np.matmul(T3,np.matmul(T4,link1_base))))
+        #  link4_top=np.matmul(T1,np.matmul(T2,np.matmul(T3,np.matmul(T4,np.matmul(T5,link1_base)))))
+        link1=np.concatenate((link1_base,link1_top),axis=1)
+        link2=np.concatenate((link2_base,link2_top),axis=1)
+        link3=np.concatenate((link3_base,link3_top),axis=1)
+        #  print(link4_top)
+        #  print(link1)
+        #  print(link2)
+        #  print(link3)
+        fig,ax=plt.subplots()
+        ax.plot(link1[0,:],link1[1,:])
+        ax.plot(link2[0,:],link2[1,:])
+        ax.plot(link3[0,:],link3[1,:])
+        ax.axis('equal')
+        ax.annotate('('+str(np.around(link3[0,1],decimals=2))+','+str(np.around(link3[1,1],decimals=2))+')',(link3[0,1],link3[1,1]))
+        plt.show()
+
+    def problem8(self):
+        len1=8
+        len2=8
+        len3=8
+        xpos=0
+        ypos=4
+        tol=0.001
+        possible_configs=[]
+        ang1=np.linspace(0,np.pi,3000)
+        ang2=np.linspace(-np.pi,np.pi,3000)
+        ang3=np.linspace(-np.pi,np.pi,3000)
+        for angle3 in ang3:
+            for angle2 in ang2:
+                value=len1**2+len2**2+len3**2+2*len1*len2*np.cos(angle2)+2*len2*len3* \
+                        np.cos(angle3)+2*len1*len3*np.cos(angle2+angle3)-xpos**2-ypos**2
+                if abs(value)<tol:
+                    for angle1 in ang1:
+                        x_value=len1*np.cos(angle1)+len2*np.cos(angle1+angle2)+len3*np.cos(angle1+angle2+angle3)
+                        y_value=len1*np.sin(angle1)+len2*np.sin(angle1+angle2)+len3*np.sin(angle1+angle2+angle3)
+                        #  print(abs(x_value-xpos),abs(y_value-ypos))
+                        if (abs(x_value-xpos)<tol) and (abs(y_value-ypos)<tol) and (angle1+angle2>0) and (angle1+angle2<np.pi):
+                            print(angle1,angle2,angle3)
+                            possible_configs.append([angle1,angle2,angle3])
+        #  print(possible_configs,len(possible_configs))
+        fig,ax=plt.subplots()
+        for config in possible_configs:
+            ang1=config[0]
+            ang2=config[1]
+            ang3=config[2]
+            T1=np.array([[np.cos(ang1),-np.sin(ang1),0],
+                [np.sin(ang1),np.cos(ang1),0],
+                [0,0,1]])
+            T2=np.array([[np.cos(ang2),-np.sin(ang2),len1],
+                [np.sin(ang2),np.cos(ang2),0],
+                [0,0,1]])
+            T3=np.array([[np.cos(ang3),-np.sin(ang3),len2],
+                [np.sin(ang3),np.cos(ang3),0],
+                [0,0,1]])
+            T4=np.array([[1,0,len3],
+                [0,1,0],
+                [0,0,1]])
+            link1_base=np.array([[0],[0],[1]])
+            link1_top=np.matmul(T1,np.matmul(T2,link1_base))
+            link2_base=copy.copy(link1_top)
+            link2_top=np.matmul(T1,np.matmul(T2,np.matmul(T3,link1_base)))
+            link3_base=copy.copy(link2_top)
+            link3_top=np.matmul(T1,np.matmul(T2,np.matmul(T3,np.matmul(T4,link1_base))))
+            link1=np.concatenate((link1_base,link1_top),axis=1)
+            link2=np.concatenate((link2_base,link2_top),axis=1)
+            link3=np.concatenate((link3_base,link3_top),axis=1)
+            #  print(link4_top)
+            #  print(link1)
+            #  print(link2)
+            #  print(link3)
+            ax.plot(link1[0,:],link1[1,:],color='C0')
+            ax.plot(link2[0,:],link2[1,:],color='C1')
+            ax.plot(link3[0,:],link3[1,:],color='C2')
+            ax.axis('equal')
+            #  ax.annotate('('+str(np.around(link3[0,1],decimals=2))+','+str(np.around(link3[1,1],decimals=2))+')',(link3[0,1],link3[1,1]))
+        plt.show()
+
+    def inverse(self,len1,len2,len3,xpos,ypos):
+        tol=0.005
+        possible_configs=[]
+        ang1=np.linspace(0,np.pi,1000)
+        ang2=np.linspace(-np.pi,np.pi,1000)
+        ang3=np.linspace(-np.pi,np.pi,1000)
+        for angle3 in ang3:
+            for angle2 in ang2:
+                value=len1**2+len2**2+len3**2+2*len1*len2*np.cos(angle2)+2*len2*len3* \
+                        np.cos(angle3)+2*len1*len3*np.cos(angle2+angle3)-xpos**2-ypos**2
+                if abs(value)<tol:
+                    for angle1 in ang1:
+                        x_value=len1*np.cos(angle1)+len2*np.cos(angle1+angle2)+len3*np.cos(angle1+angle2+angle3)
+                        y_value=len1*np.sin(angle1)+len2*np.sin(angle1+angle2)+len3*np.sin(angle1+angle2+angle3)
+                        #  print(abs(x_value-xpos),abs(y_value-ypos))
+                        if (abs(x_value-xpos)<tol) and (abs(y_value-ypos)<tol) and (angle1+angle2>0) and (angle1+angle2<np.pi):
+                            print(angle1,angle2,angle3)
+                            possible_configs.append([angle1,angle2,angle3])
+        #  print(possible_configs,len(possible_configs))
+        if len(possible_configs)==0:
+            print("That point isn't reachable")
+        fig,ax=plt.subplots()
+        for config in possible_configs:
+            ang1=config[0]
+            ang2=config[1]
+            ang3=config[2]
+            T1=np.array([[np.cos(ang1),-np.sin(ang1),0],
+                [np.sin(ang1),np.cos(ang1),0],
+                [0,0,1]])
+            T2=np.array([[np.cos(ang2),-np.sin(ang2),len1],
+                [np.sin(ang2),np.cos(ang2),0],
+                [0,0,1]])
+            T3=np.array([[np.cos(ang3),-np.sin(ang3),len2],
+                [np.sin(ang3),np.cos(ang3),0],
+                [0,0,1]])
+            T4=np.array([[1,0,len3],
+                [0,1,0],
+                [0,0,1]])
+            link1_base=np.array([[0],[0],[1]])
+            link1_top=np.matmul(T1,np.matmul(T2,link1_base))
+            link2_base=copy.copy(link1_top)
+            link2_top=np.matmul(T1,np.matmul(T2,np.matmul(T3,link1_base)))
+            link3_base=copy.copy(link2_top)
+            link3_top=np.matmul(T1,np.matmul(T2,np.matmul(T3,np.matmul(T4,link1_base))))
+            link1=np.concatenate((link1_base,link1_top),axis=1)
+            link2=np.concatenate((link2_base,link2_top),axis=1)
+            link3=np.concatenate((link3_base,link3_top),axis=1)
+            #  print(link4_top)
+            #  print(link1)
+            #  print(link2)
+            #  print(link3)
+            ax.plot(link1[0,:],link1[1,:],color='C0')
+            ax.plot(link2[0,:],link2[1,:],color='C1')
+            ax.plot(link3[0,:],link3[1,:],color='C2')
+            ax.axis('equal')
+            ax.annotate('('+str(np.around(config[0],decimals=2))+','+str(np.around(config[1],decimals=2))+','+str(np.around(config[2],decimals=2))+')',(link2[0,1],link2[1,1]))
         plt.show()
 
 
-
-def make_grid_world(problem):
+def make_grid_world(problem,sizing):
     if problem==1:
-        sizing=10
         grid=np.zeros((15*sizing,15*sizing))
         grid[10*sizing,10*sizing]=10
         grid[0*sizing,0*sizing]=5
@@ -406,7 +557,6 @@ def make_grid_world(problem):
         grid[12*sizing:13*sizing,5*sizing:13*sizing]=1
         grid[6*sizing:12*sizing,5*sizing:6*sizing]=1
     elif problem==2:
-        sizing=10
         shift=7
         grid=np.zeros((43*sizing,15*sizing))
         grid[(35+shift)*sizing,(shift+0)*sizing]=10
@@ -426,19 +576,49 @@ if __name__ == '__main__':
     parser=argparse.ArgumentParser()
     parser.add_argument("problem",help="which problem do you want to run? 9 or 10?",type=int)
     args=parser.parse_args()
-    if args.problem==9:
-        grid1=make_grid_world(1)
-        grid2=make_grid_world(2)
+    if args.problem==8:
+        arm=Arm()
+        arm.problem8()
+    elif args.problem==9:
+        sizing=25
+        grid1=make_grid_world(1,sizing)
+        grid2=make_grid_world(2,sizing)
         bug=Bug(grid1)
         bug.bug1()
-        bug.graph_path(10,1)
+        print('Bug 1 W1 Length:',np.sum(bug.bug_path)/sizing)
+        bug.graph_path(sizing,1)
         bug.bug2()
-        bug.graph_path(10,1)
+        print('Bug 2 W1 Length:',np.sum(bug.bug_path)/sizing)
+        bug.graph_path(sizing,1)
         bug=Bug(grid2)
         bug.bug1()
-        bug.graph_path(10,2)
+        print('Bug 1 W2 Length:',np.sum(bug.bug_path)/sizing)
+        bug.graph_path(sizing,2)
         bug.bug2()
-        bug.graph_path(10,2)
+        print('Bug 2 W2 Length:',np.sum(bug.bug_path)/sizing)
+        bug.graph_path(sizing,2)
     elif args.problem==10:
-        print("not ready yet")
+        arm=Arm()
+        method=input('Do you want kinematics or inverse kinematics? ')
+        if method=='kinematics':
+            len1,len2,len3=input('I need 3 link lengths. length1, length2, length3: ').split(',')
+            len1=float(len1)
+            len2=float(len2)
+            len3=float(len3)
+            ang1,ang2,ang3=input('Now I need 3 angles in degrees. angle1, angle2, angle3: ').split(',')
+            ang1=float(ang1)
+            ang2=float(ang2)
+            ang3=float(ang3)
+            arm.kinematics(len1,len2,len3,ang1,ang2,ang3)
+        elif method=='inverse kinematics':
+            len1,len2,len3=input('I need 3 link lengths. length1, length2, length3: ').split(',')
+            len1=float(len1)
+            len2=float(len2)
+            len3=float(len3)
+            xpos,ypos=input('Now I need a position for the end point: x_pos, y_pos: ').split(',')
+            xpos=float(xpos)
+            ypos=float(ypos)
+            arm.inverse(len1,len2,len3,xpos,ypos)
+        else:
+            print("Not a valid option")
 
