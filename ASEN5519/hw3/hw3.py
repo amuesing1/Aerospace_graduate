d9c25b33899438b000a51b9c0d36e0f711085414
Jeremy <amuesing1@gmail.com>
2019-11-06 15:03:18 -0700

    speed up PRM

--- ASEN5519/hw3/hw3.py
+++ ASEN5519/hw3/hw3.py
@@ -5,9 +5,10 @@ import argparse
 import sys
 import copy
 from tqdm import tqdm
+import time
 
 class A_star():
-    def construct_path(self,G,start,end,h):
+    def construct_path(self,G,start,end,h,debug=False):
         V=G[0]
         E=G[1]
         W=G[2]
@@ -41,7 +42,8 @@ class A_star():
                 while current in came_from.keys():
                     current=tuple(came_from[current])
                     total_path.insert(0,list(current))
-                print("We did it",total_path)
+                if debug:
+                    print("We did it",total_path)
                 return total_path
 
             #  print(current,f_score,g_score,came_from,open_set)
@@ -66,38 +68,48 @@ class A_star():
                     if neighbor not in open_set:
                         open_set.append(neighbor)
                 count+=1
-        print("Not possible")
+        if debug:
+            print("Not possible")
         return 1
-
-    def plot_path(self):
-        pass
 
 class PRM():
     def solve(self,n,r,obs,x_dim,y_dim,start,end,smoothing=False):
-        V=np.array([start,end])
+        self.V=np.array([start,end])
         #  print(V)
-        V=np.append(V,self.sample_space(n,obs,x_dim,y_dim),axis=0)
+        self.V=np.append(self.V,self.sample_space(n,obs,x_dim,y_dim),axis=0)
         #  print(V)
-        edges=[]
-        W=[]
-        for point in V:
-            for other in V:
+        self.edges=[]
+        self.W=[]
+        count1=0
+        for point in self.V:
+            count2=0
+            for other in self.V:
                 if list(point)==list(other):
                     continue
-                if (self.d(point,other)<r) and not (self.edge_exists(point,other,edges)):
+                if count2<count1:
+                    count2+=1
+                    continue
+                #  if self.V.index(other)<self.V.index(point):
+                #      continue
+                #  if (self.d(point,other)<r) and not (self.edge_exists(point,other,self.edges)):
+                if (self.d(point,other)<r):
                     if not self.edge_collision(point,other,obs):
-                        edges.append([list(point),list(other)])
-                        W.append(self.d(point,other))
+                        self.edges.append([list(point),list(other)])
+                        self.W.append(self.d(point,other))
+                count2+=1
+            count1+=1
         #  print(edges)
         a=A_star()
-        h=[0]*len(V)
-        V=[list(x) for x in V]
+        h=[0]*len(self.V)
+        self.V=[list(x) for x in self.V]
         #  print(V)
         #  sys.exit()
-        path=a.construct_path([V,edges,W],start,end,h)
+        self.path=a.construct_path([self.V,self.edges,self.W],start,end,h)
+        if self.path==1:
+            return 0
         if smoothing:
-            path=self.smooth_path(path,obs)
-        self.plot_path(obs,x_dim,y_dim,start,end,V,edges,path)
+            self.path=self.smooth_path(path,obs)
+        return 1
 
     def sample_space(self,n,obs,x_dim,y_dim):
         x_sam=np.random.uniform(x_dim[0],x_dim[1],n) 
@@ -136,15 +148,16 @@ class PRM():
     def d(self,point1,point2):
         return np.sqrt((point1[0]-point2[0])**2+(point1[1]-point2[1])**2)
 
-    def edge_exists(self,point1,point2,edges):
-        #  print(edges)
-        for edge in edges:
-            #  print(edge[0],point1)
-            if (list(point1)==edge[0]) and (list(point2)==edge[1]):
-                return 1
-            if (list(point1)==edge[1]) and (list(point2)==edge[0]):
-                return 1
-        return 0
+    #  def edge_exists(self,point1,point2,edges):
+    #      #  print(edges)
+    #      for edge in edges:
+    #          #  print(edge[0],point1)
+    #          if (list(point1)==edge[0]) and (list(point2)==edge[1]):
+    #              return 1
+    #              print('yes')
+    #          if (list(point1)==edge[1]) and (list(point2)==edge[0]):
+    #              return 1
+    #      return 0
 
     def edge_collision(self,point1,point2,obs):
         edge_points_x=np.linspace(point1[0],point2[0])
@@ -167,24 +180,27 @@ class PRM():
                 del path[min_ind+1:max_ind]
         return path
 
-    def plot_path(self,obs,x_dim,y_dim,start,end,V,edges,path):
+    def plot_path(self,obs,x_dim,y_dim,start,end):
         fig,ax=plt.subplots()
         for obstacle in obs:
             ob=plt.Polygon(obstacle,color='k')
             ax.add_patch(ob)
         ax.set_xlim(x_dim)
         ax.set_ylim(y_dim)
-        x_scatter=[x[0] for x in V]
-        y_scatter=[y[1] for y in V]
+        x_scatter=[x[0] for x in self.V]
+        y_scatter=[y[1] for y in self.V]
         ax.scatter(x_scatter,y_scatter)
         ax.scatter(start[0],start[1],marker='*',color='green')
         ax.scatter(end[0],end[1],marker='*',color='red')
-        for edge in edges:
+        for edge in self.edges:
             #  print(edge[0],edge[1])
             ax.plot([edge[0][0],edge[1][0]],[edge[0][1],edge[1][1]],color='C0')
-        for i in range(1,len(path)):
-            ax.plot([path[i-1][0],path[i][0]],[path[i-1][1],path[i][1]],linewidth=3,color='C1')
+        path_length=0
+        for i in range(1,len(self.path)):
+            ax.plot([self.path[i-1][0],self.path[i][0]],[self.path[i-1][1],self.path[i][1]],linewidth=3,color='C1')
+            path_length+=self.d(self.path[i-1],self.path[i])
         ax.axis('equal')
+        plt.title('Path Length=%.2f'%path_length)
         plt.show()
 
 
@@ -205,9 +221,53 @@ def problem1():
     #  a.plot_path()
 
 def problem2():
+    #part a
+    #part i
     p=PRM()
+    x_dim=[-1,11]
+    y_dim=[-3,3]
+    start=[0,0]
+    end=[10,0]
     obs1=[[[3.5,0.5],[3.5,1.5],[4.5,1.5],[4.5,0.5]],[[6.5,-1.5],[6.5,-0.5],[7.5,-0.5],[7.5,-1.5]]]
-    p.solve(200,1,obs1,[-1,11],[-3,3],[0,0],[10,0],smoothing=True)
+    p.solve(200,1,obs1,x_dim,y_dim,start,end)
+    #  p.plot_path(obs1,x_dim,y_dim,start,end)
+    #  sys.exit()
+
+    #part ii
+    n=[200,200,200,200,500,500,500,500]
+    r=[.5,1,1.5,2,.5,1,1.5,2]
+    #  n=[200]
+    #  r=[2]
+    all_sol=[]
+    all_paths=[]
+    all_times=[]
+    num_sam=5
+    for i in range(len(n)):
+        path_lengths=[np.nan]*num_sam
+        times=[np.nan]*num_sam
+        for j in tqdm(range(num_sam),ncols=100):
+            start_time=time.time()
+            result=p.solve(n[i],r[i],obs1,x_dim,y_dim,start,end)
+            times[j]=time.time()-start_time
+            if result==1:
+                path_length=0
+                for k in range(1,len(p.path)):
+                    path_length+=p.d(p.path[k-1],p.path[k])
+                path_lengths[j]=path_length
+        if np.isnan(np.nanmean(path_lengths)):
+            path_lengths_clean=[0]*num_sam
+        print(np.nanmean(path_lengths))
+        print(np.nanmean(times))
+        path_lengths_clean=[x for x in path_lengths if not np.isnan(x)]
+        all_paths.append(path_lengths_clean)
+        all_times.append(times)
+
+    plt.figure()
+    plt.boxplot(all_paths)
+
+    plt.figure()
+    plt.boxplot(all_times)
+    #  plt.show()
 
 if __name__ == '__main__':
     parser=argparse.ArgumentParser()
